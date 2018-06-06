a311978f53bfebf2587afd8e8b12f16f3bf56405
pabLOCO <pabloco@outlook.com>
2018-06-06 12:59:31 -0600

    1. agregar archivos

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Proyecto Web en Git</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div class="center">
+
+			<h1>para GITHUB </h1>
+			<br>
+			<br>
+			<br>
+
+			se van a incluir los archivos junto con la carpeta
+
+			<h1>Este titulo va a ir en el primer commit pero en Github</h1>
+			Esto es una linea iniciallllll<br>
+			se van a incluir los archivos junto con la carpeta
+			<hr>
+			esto también se ha agregado
+			<br>
+			<br>
+			<div class="center">Test es otro div</div><!--end .center -->
+			<hr>
+			<br>
+			<div class="center">Test es otro div</div><!--end .center -->
+			<hr>
+			<div class="center">Este es otro cambio</div><!--end .center -->
+			<hr>
+			<a href="http://www.infotechcr.com"></a>
+			<h1>Este titulo va a ir en el primer commit pero en Github</h1>
+			Esto es una linea iniciallllll<br>
+			se van a incluir los archivos junto con la carpeta
+			<hr>
+			esto también se ha agregado
+			<br>
+			<br>
+			<div class="center">Test es otro div</div><!--end .center -->
+			<hr>
+			<br>
+			<div class="center">Test es otro div</div><!--end .center -->
+			<hr>
+			<div class="center">Este es otro cambio</div><!--end .center -->
+			<hr>
+			<a href="http://www.infotechcr.com"></a>
+			esto es un cambio
+		</div><!--end .center -->
+		<br>
+	</body>
+</html>
